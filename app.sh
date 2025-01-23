a37da3b54aa877cdd1cbadd130e9bf7e3acdb898
Kashish742 <kashish9300@gmail.com>
2025-01-23 14:29:25 +0530

    Update app.sh

--- app.sh
+++ app.sh
@@ -1,1 +1,7 @@
-streamlit run script2.py --server.address=0.0.0.0 --server.port=8501
+#!/bin/bash
+# Activate virtual environment if required (optional)
+# source venv/bin/activate
+
+# Run the Streamlit app
+streamlit run script2.py --server.address=0.0.0.0 --server.port=8501 --server.enableCORS=false --server.enableXsrfProtection=false
+
