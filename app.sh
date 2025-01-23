c16c6a228320d1549687e578a45e7858b8eeb1ba
Kashish742 <kashish9300@gmail.com>
2025-01-23 15:09:31 +0530

    Update app.sh

--- app.sh
+++ app.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-# Activate virtual environment if required (optional)
-# source venv/bin/activate
 
-# Run the Streamlit app
-streamlit run script2.py --server.address=0.0.0.0 --server.port=8501 --server.enableCORS=false --server.enableXsrfProtection=false
+# Run the Streamlit application
+# Using port 8080 as it's the only available option in CERN Paas
+streamlit run script2.py --server.port=8080 --server.address=0.0.0.0 --server.enableCORS=false --server.enableXsrfProtection=false
+
 
